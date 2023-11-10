affb2dc93b0784733f2ef4deb191a0b1eb10eccb
Colin Robinson <colin@Colins-Air.lan>
2023-11-09 23:32:51 -0600

    Worked on Task 3 and 4.  Still need to resolve issue in Task 2 and finish some code in Tasks 3 and 4.

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,33 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
+    @NotNull
+    @NotBlank
+    @Size(max = 250)
+    private String location;
+
+    public Employer() {}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @OneToMany
+    @JoinColumn(name = "employer_id")
+    private final List<Job> jobs = new ArrayList<>();
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,37 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Skill extends AbstractEntity {
+    @NotBlank
+    @NotNull
+    @Size(max = 500)
+    private String description;
+
+    public Skill() {}
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @ManyToMany(mappedBy = "skills")
+    private final List<Job> jobs = new ArrayList<>();
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
 
 }
